40b58ee526c6f1cb16f2bcf3c2d0aa714c50ec54
Clint Olson <coandco@gmail.com>
2021-11-30 22:50:50 -0800

    Use native zip method rather than external more-itertools module

--- advent2021_day01.py
+++ advent2021_day01.py
@@ -1,11 +1,10 @@
 from utils import read_data
-from more_itertools import windowed
 from typing import Iterable
 
 
 def count_increases(data: Iterable) -> int:
     num_increases = 0
-    for first, second in windowed(data, 2):
+    for first, second in zip(data, data[1:]):
         if second > first:
             num_increases += 1
     return num_increases
@@ -13,6 +12,6 @@ def count_increases(data: Iterable) -> int:
 
 if __name__ == '__main__':
     part_one_input = [int(x) for x in read_data().splitlines()]
-    part_two_input = [sum(x) for x in windowed(part_one_input, 3)]
+    part_two_input = [sum(x) for x in zip(part_one_input, part_one_input[1:], part_one_input[2:])]
     print(f"Part one: {count_increases(part_one_input)}")
     print(f"Part two: {count_increases(part_two_input)}")
