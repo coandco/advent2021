870bcdd330bd9267aa9c238d7aee33eec44f5092
Clint Olson <coandco@gmail.com>
2021-12-01 22:23:23 -0800

    Eliminate need for start= parameter on sum

--- advent2021_day02.py
+++ advent2021_day02.py
@@ -12,6 +12,11 @@ class Coord(NamedTuple):
     def __mul__(self, amount: int) -> 'Coord':
         return Coord(x=self.x * amount, y=self.y * amount)
 
+    def __radd__(self, other: int) -> 'Coord':
+        if other == 0:
+            return self
+        raise NotImplemented
+
     @staticmethod
     def from_str(line: str):
         direction, distance = line.split(" ", maxsplit=1)
@@ -24,10 +29,15 @@ class AimedCoord(NamedTuple):
     aim: int
 
     def __add__(self, other: 'AimedCoord') -> 'AimedCoord':
-        return AimedCoord(x=self.x + other.x, y=self.y + (other.y * other.x * self.aim), aim=self.aim + other.aim)
+        return AimedCoord(x=self.x + other.x, y=self.y + (other.y    * other.x * self.aim), aim=self.aim + other.aim)
 
     def __mul__(self, amount: int):
         return AimedCoord(x=self.x * amount, y=self.y, aim=self.aim*amount)
+
+    def __radd__(self, other: int) -> 'AimedCoord':
+        if other == 0:
+            return self
+        raise NotImplemented
 
     @staticmethod
     def from_str(line: str):
@@ -50,9 +60,9 @@ PART_TWO_DIRECTIONS = {
 
 if __name__ == '__main__':
     PART_ONE_INPUT = [Coord.from_str(line) for line in read_data().splitlines()]
-    part_one_position = sum(PART_ONE_INPUT, start=Coord(0, 0))
+    part_one_position = sum(PART_ONE_INPUT)
     print(f"Part one: {part_one_position.x * part_one_position.y}")
 
     PART_TWO_INPUT = [AimedCoord.from_str(line) for line in read_data().splitlines()]
-    part_two_position = sum(PART_TWO_INPUT, start=AimedCoord(0, 0, 0))
+    part_two_position = sum(PART_TWO_INPUT)
     print(f"Part two: {part_two_position.x * part_two_position.y}")
