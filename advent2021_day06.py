2438d5c9367ab89d7efbdf6477cb2b4bfb91287f
Clint Olson <coandco@gmail.com>
2021-12-05 21:57:14 -0800

    Add solution for day 6

---
+++ advent2021_day06.py
@@ -0,0 +1,24 @@
+from utils import read_data
+from collections import Counter
+
+
+def run_cycle(current_state: Counter) -> Counter:
+    new_state = Counter()
+    for i in range(1, 9):
+        new_state[i-1] = current_state.get(i, 0)
+    new_state[6] += current_state[0]
+    new_state[8] = current_state[0]
+    return new_state
+
+
+if __name__ == '__main__':
+    INPUT = [int(x) for x in read_data().split(",")]
+    state = Counter(INPUT)
+    for _ in range(80):
+        state = run_cycle(state)
+    print(f"Part one: {sum(state.values())}")
+
+    state = Counter(INPUT)
+    for _ in range(256):
+        state = run_cycle(state)
+    print(f"Part two: {sum(state.values())}")
