d9dec29fb6d9d6b6d8a5588c7258fe93f8be82b4
Clint Olson <coandco@gmail.com>
2021-12-07 11:44:32 -0800

    Check the whole range rather than only existing positions for day 7

--- advent2021_day07.py
+++ advent2021_day07.py
@@ -5,11 +5,9 @@ from collections import Counter
 def get_cost(counts: Counter, position_to_check: int, part_two: bool = False) -> int:
     cost = 0
     for position, count in counts.items():
-        distance = abs(position - position_to_check)
+        single_cost = abs(position - position_to_check)
         if part_two:
-            single_cost = distance * (distance + 1) // 2
-        else:
-            single_cost = distance
+            single_cost = single_cost * (single_cost + 1) // 2
         cost += single_cost * count
     return cost
 
@@ -17,7 +15,7 @@ def get_cost(counts: Counter, position_to_check: int, part_two: bool = False) ->
 if __name__ == '__main__':
     INPUT = [int(x) for x in read_data().split(",")]
     counts = Counter(INPUT)
-    part_one_costs = {x: get_cost(counts, x) for x in counts}
+    part_one_costs = {x: get_cost(counts, x) for x in range(min(counts.keys()), max(counts.keys())+1)}
     print(f"Part one: {min(part_one_costs.values())}")
-    part_two_costs = {x: get_cost(counts, x, part_two=True) for x in counts}
+    part_two_costs = {x: get_cost(counts, x, part_two=True) for x in range(min(counts.keys()), max(counts.keys())+1)}
     print(f"Part two: {min(part_two_costs.values())}")
