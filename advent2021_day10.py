45a9ffa39f8d45ca4b3f3c036dfdc66f5c76f70c
Clint Olson <coandco@gmail.com>
2021-12-09 22:05:28 -0800

    Refine day 10 to a single pass

--- advent2021_day10.py
+++ advent2021_day10.py
@@ -1,7 +1,12 @@
 from utils import read_data
 from collections import deque
 from statistics import median
-from typing import Optional
+from typing import Tuple, NamedTuple
+
+
+class PointValue(NamedTuple):
+    value: int
+    corrupted: bool
 
 
 CELL_CLOSERS = {
@@ -26,7 +31,7 @@ POINT_VALUES_TWO = {
 }
 
 
-def part_one(line: str) -> int:
+def score_line(line: str) -> PointValue:
     stack = deque()
     for char in line:
         if char in CELL_CLOSERS.keys():
@@ -34,28 +39,16 @@ def part_one(line: str) -> int:
         elif char in CELL_CLOSERS.values():
             expected_char = stack.pop()
             if char != expected_char:
-                return POINT_VALUES_ONE[char]
-    return 0
-
-
-def part_two(line: str) -> int:
-    stack = deque()
-    for char in line:
-        if char in CELL_CLOSERS.keys():
-            stack.append(CELL_CLOSERS[char])
-        elif char in CELL_CLOSERS.values():
-            expected_char = stack.pop()
-            if char != expected_char:
-                return 0
+                return PointValue(POINT_VALUES_ONE[char], corrupted=True)
     score = 0
     while stack:
         score *= 5
         score += POINT_VALUES_TWO[stack.pop()]
-    return score
+    return PointValue(score, corrupted=False)
 
 
 if __name__ == '__main__':
     INPUT = read_data().splitlines()
-    print(f"Part one: {sum(part_one(x) for x in INPUT)}")
-    part_two_data = median(points for x in INPUT if (points := part_two(x)) != 0)
-    print(f"Part two: {part_two_data}")
+    points = [score_line(x) for x in INPUT]
+    print(f"Part one: {sum(x.value for x in points if x.corrupted is True)}")
+    print(f"Part two: {median(x.value for x in points if x.corrupted is False)}")
