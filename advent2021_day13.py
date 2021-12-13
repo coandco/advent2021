3be7a119da029e4be3b3ca4ac45058dd4d125503
Clint Olson <coandco@gmail.com>
2021-12-13 15:22:45 -0800

    Clean up day 13

--- advent2021_day13.py
+++ advent2021_day13.py
@@ -13,6 +13,43 @@ class Fold(NamedTuple):
     at: int
 
 
+# Taken from the advent-of-code-ocr package
+ALPHABET_6 = {
+    " ## \n#  #\n#  #\n####\n#  #\n#  #": "A",
+    "### \n#  #\n### \n#  #\n#  #\n### ": "B",
+    " ## \n#  #\n#   \n#   \n#  #\n ## ": "C",
+    "####\n#   \n### \n#   \n#   \n####": "E",
+    "####\n#   \n### \n#   \n#   \n#   ": "F",
+    " ## \n#  #\n#   \n# ##\n#  #\n ###": "G",
+    "#  #\n#  #\n####\n#  #\n#  #\n#  #": "H",
+    " ###\n  # \n  # \n  # \n  # \n ###": "I",
+    "  ##\n   #\n   #\n   #\n#  #\n ## ": "J",
+    "#  #\n# # \n##  \n# # \n# # \n#  #": "K",
+    "#   \n#   \n#   \n#   \n#   \n####": "L",
+    " ## \n#  #\n#  #\n#  #\n#  #\n ## ": "O",
+    "### \n#  #\n#  #\n### \n#   \n#   ": "P",
+    "### \n#  #\n#  #\n### \n# # \n#  #": "R",
+    " ###\n#   \n#   \n ## \n   #\n### ": "S",
+    "#  #\n#  #\n#  #\n#  #\n#  #\n ## ": "U",
+    "#   \n#   \n # #\n  # \n  # \n  # ": "Y",
+    "####\n   #\n  # \n #  \n#   \n####": "Z",
+}
+
+
+# Also adapted from advent-of-code-ocr
+def parse_output(raw: str) -> str:
+    lines = raw.splitlines()
+    line_length = len(lines[0])
+    parsed_output = ""
+    low, high = 0, 4
+    while high <= line_length:
+        single_letter = ALPHABET_6["\n".join([x[low:high] for x in lines])]
+        parsed_output += single_letter
+        low += 5
+        high += 5
+    return parsed_output
+
+
 def parse_input(data: str) -> Tuple[Set[Coord], List[Fold]]:
     coord_str, folds_str = data.split("\n\n", 1)
     coords = set()
@@ -52,18 +89,15 @@ def output_dots(dots: Set[Coord]):
         max_y = dot.y if dot.y > max_y else max_y
     output = []
     for y in range(min_y, max_y+1):
-        line = ""
-        for x in range(min_x, max_x+1):
-            char = "#" if Coord(x=x, y=y) in dots else " "
-            line += char
+        line = "".join("#" if Coord(x=x, y=y) in dots else " " for x in range(min_x, max_x+1))
         output.append(line)
     return "\n".join(output)
 
 
-dots, folds = parse_input(read_data())
-part_one_dots = do_fold(dots, folds[0])
-print(f"Part one: {len(part_one_dots)}")
-for fold in folds:
-    dots = do_fold(dots, fold)
-print(f"Part two:\n{output_dots(dots)}")
-
+if __name__ == '__main__':
+    dots, folds = parse_input(read_data())
+    part_one_dots = do_fold(dots, folds[0])
+    print(f"Part one: {len(part_one_dots)}")
+    for fold in folds:
+        dots = do_fold(dots, fold)
+    print(f"Part two: {parse_output(output_dots(dots))}")
