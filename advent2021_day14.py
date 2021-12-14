537591adecb99a81ea2c04cd87242d155d346748
Clint Olson <coandco@gmail.com>
2021-12-13 23:36:12 -0800

    Clean up day 14

--- advent2021_day14.py
+++ advent2021_day14.py
@@ -1,5 +1,5 @@
 from utils import read_data
-from typing import Tuple, Dict, Iterable
+from typing import Tuple, Dict
 from collections import Counter
 
 
@@ -12,24 +12,19 @@ def parse_input(data: str) -> Tuple[str, Dict[str, str]]:
     return template, rules
 
 
-def get_pairs(polymer: str) -> Iterable[str]:
-    for i in range(len(polymer)-1):
-        yield polymer[i:i+2]
-
-
 def run_round_countwise(rules: Dict[str, str], letters: Counter, pairs: Counter) -> Tuple[Counter, Counter]:
     new_pairs = Counter()
     for pair in pairs.keys():
-        if pair in rules:
-            new_pairs[pair[0] + rules[pair]] += pairs[pair]
-            new_pairs[rules[pair] + pair[1]] += pairs[pair]
-            letters[rules[pair]] += pairs[pair]
+        # Used to have an "if pair in rules" here, but there are no cache misses in our data
+        new_pairs[pair[0] + rules[pair]] += pairs[pair]
+        new_pairs[rules[pair] + pair[1]] += pairs[pair]
+        letters[rules[pair]] += pairs[pair]
     return letters, new_pairs
 
 
 if __name__ == '__main__':
     template, rules = parse_input(read_data())
-    letter_counts, pair_counts = Counter(template), Counter(get_pairs(template))
+    letter_counts, pair_counts = Counter(template), Counter(template[x:x+2] for x in range(len(template)-1))
     for _ in range(10):
         letter_counts, pair_counts = run_round_countwise(rules, letter_counts, pair_counts)
     print(f"Part one: {max(letter_counts.values()) - min(letter_counts.values())}")
