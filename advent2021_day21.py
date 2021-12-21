598f62b2e2cb9758317f09f85deb252a2d89798b
Clint Olson <coandco@gmail.com>
2021-12-21 10:23:24 -0800

    Convert day 21 part 1 to use my part-two mutate, and add comments

--- advent2021_day21.py
+++ advent2021_day21.py
@@ -2,42 +2,20 @@ from utils import read_data
 from typing import Generator, List, Tuple, Dict, Optional
 
 
+# WorldState: player_locs, player_scores, turn_index
+WorldState = Tuple[Tuple[int, int], Tuple[int, int], int]
+
+
 def fixed_die(start: int = 1, sides: int = 100) -> Generator[int, None, None]:
     while True:
         yield from range(start, sides+1)
 
 
-def part_one(player_locs: List[int]) -> int:
-    player_locs = player_locs[:]
-    player_scores = [0] * len(player_locs)
-    die = fixed_die()
-    num_rolls = 0
-    done = False
-    while True:
-        for i in range(len(player_locs)):
-            rolls = (next(die), next(die), next(die))
-            player_locs[i] = (player_locs[i] + sum(rolls)) % 10
-            player_scores[i] += player_locs[i] + 1  # +1 because 1-10 instead of 0-9
-            num_rolls += 3
-            if player_scores[i] >= 1000:
-                done = True
-                break
-        if done:
-            break
-    return min(player_scores) * num_rolls
-
-
-WorldState = Tuple[Tuple[int, int], Tuple[int, int], int]
-
-
-# roll distribution for three rolls
-ROLLS = {6: 7, 5: 6, 7: 6, 4: 3, 8: 3, 3: 1, 9: 1}
-
-
-def mutate_part_two(state: WorldState, roll: int) -> WorldState:
+def mutate(state: WorldState, roll: int) -> WorldState:
     locs, scores, pnum = state
 
     new_loc = (locs[pnum] + roll) % 10
+    # We add one to the score because we're mapping spots 1-10 onto 0-9 so we can do modulo on them
     new_score = scores[pnum] + new_loc + 1
     if pnum:
         return (locs[not pnum], new_loc), (scores[not pnum], new_score), int(not pnum)
@@ -53,26 +31,51 @@ def state_winner(state: WorldState, limit: int = 21) -> Optional[int]:
     return None
 
 
+def part_one(starting_locs: List[int]) -> int:
+    # state: player_locs, player_scores, turn_index
+    state: WorldState = ((starting_locs[0], starting_locs[1]), (0, 0), 0)
+    die = fixed_die()
+    num_rolls = 0
+    while (winner := state_winner(state, limit=1000)) is None:
+        state = mutate(state, next(die) + next(die) + next(die))
+        num_rolls += 3
+    return state[1][not winner] * num_rolls
+
+
+# Roll distribution for three rolls of the quantum die.
+# Each time the three quantum dice are rolled, it creates 27 new universes.  However, the
+# universes where the rolls add up to the same number are indistinguishable -- thus, universes
+# where it rolls 1+2+3 are the same as the universes where it rolls 3+2+1.  We can therefore
+# generate a static dict of the number of universes for each roll total via the following method:
+# ROLLS = {}
+# for x in range(1, 4):
+#     for y in range(1, 4):
+#         for z in range(1, 4):
+#             ROLLS[x+y+z] = ROLLS.get(x+y+z, 0) + 1
+ROLLS = {6: 7, 5: 6, 7: 6, 4: 3, 8: 3, 3: 1, 9: 1}
+
+
 def part_two(starting_locs: List[int]) -> int:
     states: Dict[WorldState, int] = {((starting_locs[0], starting_locs[1]), (0, 0), 0): 1}
     winning_states: List[int] = [0, 0]
 
     while states:
-        # Python dicts are ordered since 3.5, and this will get the first-inserted item from the list and delete it
+        # Python dicts are ordered since 3.5, and this will get the first-inserted item from the dict and delete it
         state, universes = next(iter(states.items()))
         del states[state]
 
-        if (winner := state_winner(state)) is not None:
+        if (winner := state_winner(state, limit=21)) is not None:
             winning_states[winner] += universes
             continue
 
         for roll, roll_universes in ROLLS.items():
-            new_state = mutate_part_two(state, roll)
+            new_state = mutate(state, roll)
             states[new_state] = states.get(new_state, 0) + (universes * roll_universes)
     return max(winning_states)
 
 
 if __name__ == '__main__':
-    player_locs = [int(x[-1])-1 for x in read_data().splitlines()]
+    # It's convenient to have the player locs be 0-9 instead of 1-10 so modulo works
+    player_locs = [int(x[-2:].strip())-1 for x in read_data().splitlines()]
     print(f"Part one: {part_one(player_locs)}")
     print(f"Part two: {part_two(player_locs)}")
